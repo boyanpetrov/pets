260e5877eeb9b28bd12066ce44dedf464220fa8a
boyanpetrov7 <boyan.petrov@entura.co.uk>
2018-02-20 17:34:32 +0000

    add heroku postgres db config

--- env.sh
+++ env.sh
@@ -1,1 +1,2 @@
-export DATABASE_URL="postgresql://admin:admin@localhost:5432/peta";
+export NODE_ENV=production;
+export DATABASE_URL="postgresql://lrykawszagxdtt:6ffa74164eacfe93c4149522f3bba0faac52dc7f34e5e68e3451a45bf03a7c56@ec2-54-235-90-200.compute-1.amazonaws.com:5432/d4bcv32uqk1bv2";
