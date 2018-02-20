5bb9d8485bc5c791db7495e47feb3bee8d843df5
boyanpetrov7 <boyan.petrov@entura.co.uk>
2018-02-20 17:29:06 +0000

    add production config

---
+++ server/config/production.js
@@ -0,0 +1,9 @@
+  module.exports = {
+  db: {
+    userId: 'lrykawszagxdtt',
+    password: '6ffa74164eacfe93c4149522f3bba0faac52dc7f34e5e68e3451a45bf03a7c56',
+    host: 'ec2-54-235-90-200.compute-1.amazonaws.com',
+    port: '5432',
+    database: 'd4bcv32uqk1bv2'
+  },
+};
