fb93a38b2d537570f459faf0f7bb8109f9aa19d5
boyanpetrov7 <boyan.petrov@entura.co.uk>
2018-02-21 17:29:41 +0000

    remove env.sh

--- env.sh
+++
@@ -1,2 +0,0 @@
-export NODE_ENV=production;
-export DATABASE_URL="postgresql://lrykawszagxdtt:6ffa74164eacfe93c4149522f3bba0faac52dc7f34e5e68e3451a45bf03a7c56@ec2-54-235-90-200.compute-1.amazonaws.com:5432/d4bcv32uqk1bv2";
